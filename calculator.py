eaf34465bcb40f5e5c4ca5d32c06798d416fd857
Katrina Pabuna <kinapabuna@gmail.com>
2023-05-11 19:20:42 +0800

    Import tkinter GUI

---
+++ calculator.py
@@ -0,0 +1,25 @@
+# Import tkinter GUI
+from tkinter import *
+
+# Define the buttons to function
+
+# Create the window for the calculator
+calcu = Tk()
+calcu.title("Calculator - 2023")
+
+# Display the numbers and operation
+
+# Buttons 7, 8, 9, +
+
+# Buttons 4, 5, 6, -
+
+# Buttons 1, 2, 3, *
+
+# Buttons C, 0, =, /
+
+# Create a popup window for another calculation
+
+# Create a popup window to exit the app
+
+# ===== start =====
+calcu.mainloop()
